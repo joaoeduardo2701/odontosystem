de29d1c28a88beef945f86d1d9ac9720dc1f6e98
Dudu <duducardoso2701@gmail.com>
2025-05-15 19:15:02 -0300

    página inicial da clinica criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { BeneficiosComponent } from './components/beneficios/beneficios.component';
+import { AgendarComponent } from './components/agendar/agendar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, BeneficiosComponent, AgendarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/beneficios/beneficios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beneficios',
+  imports: [],
+  templateUrl: './beneficios.component.html',
+  styleUrl: './beneficios.component.scss'
+})
+
+export class BeneficiosComponent {
+
+}
